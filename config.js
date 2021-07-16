e8663c75062b6fd07692a5cee96c9523eb4cd296
canhoanglinh <50643417+canhoanglinh@users.noreply.github.com>
2021-07-16 18:47:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const CONFIG = {
     btnNo: 'Không, ông mơ đi :3',
     question:'Em có công nhận có anh trai đẹp dzai số 1 như này là điều may mắn không?',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. THương Ỉn nhất',
+    reply: 'Đương nhiên dòi, a Linh đẹp dzai số 2 ai dám nhận số 1, gọi a Linh là hố đen vì lúc nào cũng cuốn hút <33333333',
+    mess: 'Anh biết mà 🥰. Thương Ỉn nhất',
     messDesc: 'Anh biết em gái của mình là một người biết giữ lời hứa, quân tử nhất ngôn tứ mã nan truy phải không?',
     btnAccept: 'Đúng dòiiiiiiiiiii <3',
     messLink: 'https://www.facebook.com/messages/t/100014234809013' //link mess của các bạn. VD: https://m.me/nam.nodemy
