dbd5f8684ae14a9f6e31192ffc79c7eb7a5bbc50
canhoanglinh <50643417+canhoanglinh@users.noreply.github.com>
2021-07-16 18:26:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ const CONFIG = {
     introTitle: 'Ỉn à!',
     introDesc: `Dù e có là ỉn thì vẫn là 1 con lợn nhẹ hơn cân nặng trung bình của loài lợn nên đừng buốn nhé`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Hãy trả lời trung thực nhé 😙',
+    desc: 'Em có hứa sẽ bao a Linh 1 bữa ăn và tặng quà chục tỷ khum? ',
+    btnYes: 'Dạ đương nhiên r, a Linh là số 1 <3',
+    btnNo: 'Không, ông mơ đi :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100014234809013' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
